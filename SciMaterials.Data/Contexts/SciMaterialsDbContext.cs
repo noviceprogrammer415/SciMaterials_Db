8866da60751ca4bd20de84d389a70b607056b3b7
Дмитрий <semu-dima@yandex.ru>
2022-09-26 23:02:49 +0300

    added context

---
+++ SciMaterials.Data/Contexts/SciMaterialsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SciMaterials.Data.Models;
+using SciMaterials.Data.Models.Resources;
+using File = SciMaterials.Data.Models.Resources.File;
+
+namespace SciMaterials.Data.Contexts
+{
+    public class SciMaterialsDbContext : DbContext
+    {
+        public DbSet<File> Files { get; set; }
+        public DbSet<HtmlText> HtmlTexts { get; set; }
+        public DbSet<UrlAddress> UrlAddresses { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Partition> Partitions { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        public SciMaterialsDbContext(DbContextOptions<SciMaterialsDbContext> options) : base(options)
+        {
+        }
+    }
+}
